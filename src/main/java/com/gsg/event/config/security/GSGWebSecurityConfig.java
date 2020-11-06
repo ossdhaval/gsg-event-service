94f397c556d162bfd5f756fd949b220fa24a3a5e
ossdhaval <ossdhaval@outlook.com>
2020-11-06 20:13:25 +0530

    Add spring security with default login page

---
+++ src/main/java/com/gsg/event/config/security/GSGWebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.gsg.event.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+
+@Configuration
+public class GSGWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity hs) throws Exception
+    {
+        hs.
+                headers().frameOptions().sameOrigin()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+//        .successHandler(new GSGAuthenticationSuccessHandler())
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity){
+        webSecurity.ignoring().mvcMatchers("/css/**", "/webjars/**");
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder amb) throws Exception {
+        amb.inMemoryAuthentication().withUser("dd").password("{noop}dd").roles("ADMIN");
+    }
+
+}
--- src/main/java/com/gsg/event/controller/GsgEventController.java
+++ src/main/java/com/gsg/event/controller/GsgEventController.java
@@ -1,17 +1,18 @@
 package com.gsg.event.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
 
 @RestController
 public class GsgEventController
 {
-    @RequestMapping("/")
+    @GetMapping("/mylogin")
     public String sayHello()
     {
-        return "Hello";
+        return "mylogin";
     }
-
-
 }
